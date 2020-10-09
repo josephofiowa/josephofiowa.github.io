a485e19950a64e8553eb6f92e2dbf690d80aec5a
Joseph Nelson <joseph.nelson2012@gmail.com>
2020-10-08 22:57:11 -0400

    no more JS here

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,32 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
     */
-    (function ($) {
-    "use strict"; // Start of use strict
-
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 70,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
+    // (function ($) {
+    // "use strict"; // Start of use strict
+    //
+    // // Smooth scrolling using jQuery easing
+    // $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    //     if (
+    //         location.pathname.replace(/^\//, "") ==
+    //             this.pathname.replace(/^\//, "") &&
+    //         location.hostname == this.hostname
+    //     ) {
+    //         var target = $(this.hash);
+    //         target = target.length
+    //             ? target
+    //             : $("[name=" + this.hash.slice(1) + "]");
+    //         if (target.length) {
+    //             $("html, body").animate(
+    //                 {
+    //                     scrollTop: target.offset().top - 70,
+    //                 },
+    //                 1000,
+    //                 "easeInOutExpo"
+    //             );
+    //             return false;
+    //         }
+    //     }
+    // });
 
     // // Closes responsive menu when a scroll trigger link is clicked
     // $(".js-scroll-trigger").click(function () {
